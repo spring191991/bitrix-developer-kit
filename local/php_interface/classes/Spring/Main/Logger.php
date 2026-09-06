f389e81d0fb8907554df7fa4c34e45eb72e1d2d7
spring191991 <138762497+spring191991@users.noreply.github.com>
2026-09-06 19:45:17 +0300

    Update Logger.php

--- local/php_interface/classes/Spring/Main/Logger.php
+++ local/php_interface/classes/Spring/Main/Logger.php
@@ -6,7 +6,6 @@ class Logger
 {
     public function logError($errorMessage) 
     {
-        // Здесь будет логика записи в файл
 		if(is_object($errorMessage)) $errorMessage = $errorMessage->getMessage(); 
 		$data = "[".date('d.m.Y H:i:s')."] ".$errorMessage."\n";
 		$bytesWritten = file_put_contents(Application::getDocumentRoot()."/local/logs/app_errors.log", $data, FILE_APPEND);
